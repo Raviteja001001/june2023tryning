568822ffdbc95659b22001b63dfdd72adc5a94f6
Raviteja001001 <131241321+Raviteja001001@users.noreply.github.com>
2023-06-02 17:22:43 +0530

    Add files via upload

---
+++ P.java
@@ -0,0 +1,7 @@
+class P1
+{
+public static void main(Srting args[])
+{
+System.out.println("hello world");
+}
+}
---
+++ Sum.java
@@ -0,0 +1,11 @@
+import java.io.*;
+class Sum
+{
+ public static void main(String arg[])
+  {
+  int a=20;
+  int b=10; 
+  int c=a+b;
+  System.out.println("a+b="+c);
+ }
+}
